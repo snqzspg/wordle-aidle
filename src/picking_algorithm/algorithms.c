262bac3c0eccb962e6b98573a3a2d44d830ca9bc
Snqzs' PG <43104884+snqzspg@users.noreply.github.com>
2022-03-08 09:28:08 +0800

    Update files

--- src/picking_algorithm/algorithms.c
+++ src/picking_algorithm/algorithms.c
@@ -2,52 +2,128 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "../error/print_error.h"
 #include "../terminal_helper/ccolours.h"
 #include "../terminal_helper/helper_fxs.h"
 #include "../utilities/input-helper.h"
 
 #include "algorithms.h"
 
-struct algo_registry* registered_algos = NULL;
+#define init_alloc_size 4
+
+char accepting_registration = 1;
+
+alcat* registered_algo_cats = NULL;
 size_t registered_algos_allocated = 0;
 size_t registered_algos_len = 0;
 
-struct algo_registry {
-	int id;
-	char* name;
-	char* tag_desc; // Small desc
-//	void* (*create_slvr) (test_sess* session);
-//	char (*slvr_open) (void* s, test_sess* session);
-//	const char* (*slvr_suggest) (void* s, test_sess* session);
-//	void (*slvr_receive_result) (void* s, test_sess* session, char* word, char* result);
-//	void (*delete_slvr) (void* s, test_sess* session);
-};
+static void alcat_add_algo(alcat* cat, algorithm* alg) {
+	if (cat -> registered_algorithms == NULL) {
+		cat -> registered_algorithms = malloc(sizeof(algorithm*) * init_alloc_size);
+		if (cat -> registered_algorithms == NULL) {
+			print_error_ln("ERROR no more memory to register algorithms");
+			return;
+		}
+		cat -> registered_algo_alloc = init_alloc_size;
+	}
+	if (cat -> registered_algo_count >= cat -> registered_algo_alloc) {
+		cat -> registered_algo_alloc *= 2;
+		cat -> registered_algorithms = realloc(cat -> registered_algorithms, sizeof(algorithm*) * (cat -> registered_algo_alloc));
+		if (cat -> registered_algorithms == NULL) {
+			print_error_ln("ERROR no more memory to register algorithms");
+			return;
+		}
+	}
+	cat -> registered_algorithms[cat -> registered_algo_count] = alg;
+	cat -> registered_algo_count++;
+}
 
-//void register_algorithm(int id, const char* name, const char* short_desc, void* (*create_slvr) (test_sess* session), char (*slvr_open) (void* s, test_sess* session), const char* (*slvr_suggest) (void* s, test_sess* session), void (*slvr_receive_result) (void* s, test_sess* session, char* word, char* result), void (*delete_slvr) (void* s, test_sess* session)) {
-//	if (registered_algos == NULL) {
-//		registered_algos = malloc(sizeof(struct algo_registry) * 8);
-//		registered_algos_allocated = 8;
-//	}
-//	if (registered_algos_len >= registered_algos_allocated) {
-//		registered_algos = realloc(registered_algos, sizeof(struct algo_registry) * registered_algos_allocated * 2);
-//		registered_algos_allocated *= 2;
-//	}
-//	registered_algos[registered_algos_len].id = id;
-//	registered_algos[registered_algos_len].name = malloc(sizeof(char) * (strlen(name) + 1));
-//	strcpy(registered_algos[registered_algos_len].name, name);
-//	registered_algos[registered_algos_len].tag_desc = malloc(sizeof(char) * (strlen(short_desc) + 1));
-//	strcpy(registered_algos[registered_algos_len].tag_desc, short_desc);
-//	registered_algos[registered_algos_len].create_slvr = create_slvr;
-//	registered_algos[registered_algos_len].slvr_open = slvr_open;
-//	registered_algos[registered_algos_len].slvr_suggest = slvr_suggest;
-//	registered_algos[registered_algos_len].slvr_receive_result = slvr_receive_result;
-//	registered_algos[registered_algos_len].delete_slvr = delete_slvr;
-//	registered_algos_len++;
-//}
-//
+void register_algorithm(alcat* cat, int id, const char* name, char* (*suggest_word) (gbucket* guess_board, wlist** word_lists, size_t nword_lists), void (*init) (solver* slvr, algorithm* algo), void (*cleanup) (solver* slvr, algorithm* algo), void (*guess_made) (solver* slvr, algorithm* algo)) {
+	algorithm* a = malloc(sizeof(algorithm));
+	a -> id = id;
+	a -> name = malloc(sizeof(char) * (strlen(name) + 1));
+	if (a -> name == NULL) {
+		print_error_ln("ERROR no more memory to register algorithms.");
+		return;
+	}
+	strcpy(a -> name, name);
+	a -> suggest_word = suggest_word;
+	a -> init = init;
+	a -> cleanup = cleanup;
+	a -> guess_made = guess_made;
+	alcat_add_algo(cat, a);
+}
+
 //void register_algorithms() {
 //	//
 //}
+
+void algo_category_register(const int id, const char* name) {
+	if (!accepting_registration) {
+		print_err_warning_ln("WARNING algorithm register process has ended.");
+		return;
+	}
+	if (registered_algo_cats == NULL) {
+		registered_algo_cats = (alcat*) malloc(sizeof(alcat) * init_alloc_size);
+		if (registered_algo_cats == NULL) {
+			print_error_ln("ERROR no more memory to register algorithm categories");
+			return;
+		}
+		registered_algos_allocated = init_alloc_size;
+	}
+	if (registered_algos_len >= registered_algos_allocated) {
+		registered_algos_allocated *= 2;
+		registered_algo_cats = realloc(registered_algo_cats, sizeof(alcat) * registered_algos_allocated);
+		if (registered_algo_cats == NULL) {
+			print_error_ln("ERROR no more memory to register algorithm categories");
+			return;
+		}
+	}
+	size_t i = registered_algos_len;
+	registered_algo_cats[i].id = id;
+	registered_algo_cats[i].name = malloc(sizeof(char) * (strlen(name) + 1));
+	if (registered_algo_cats[i].name == NULL) {
+		print_error_ln("ERROR no more memory to register algorithm categories");
+		return;
+	}
+	strcpy(registered_algo_cats[i].name, name);
+	registered_algo_cats[i].info = NULL;
+	registered_algo_cats[i].registered_algorithms = NULL;
+	registered_algo_cats[i].registered_algo_count = 0;
+	registered_algo_cats[i].registered_algo_alloc = 0;
+	registered_algos_len++;
+}
+
+void alcat_add_desc(alcat* cat, const char* desc) {
+	if (cat -> info == NULL) {
+		cat -> info = malloc(sizeof(char) * (strlen(desc) + 1));
+	} else {
+		cat -> info = realloc(cat -> info, sizeof(char) * (strlen(desc) + 1));
+	}
+	if (cat -> info == NULL) {
+		print_error_ln("ERROR no more memory to add category description.");
+		return;
+	}
+	strcpy(cat -> info, desc);
+}
+
+void algorithm_delete(algorithm* x) {
+	free(x -> name);
+	free(x);
+}
+
+void alcats_clear() {
+	for (size_t i = 0; i < registered_algos_len; i++) {
+		free(registered_algo_cats[i].info);
+		free(registered_algo_cats[i].name);
+		if (registered_algo_cats[i].registered_algorithms != NULL) {
+			for (size_t j = 0; j < registered_algo_cats[i].registered_algo_count; j++) {
+				algorithm_delete(registered_algo_cats[i].registered_algorithms[j]);
+			}
+		}
+	}
+	free(registered_algo_cats);
+}
 
 static void lowercase_ascii(char* s) {
 	if (s == NULL) return;
@@ -75,16 +151,24 @@ int select_algo_page(void (*print_title_stuff)(), int* algo, void (*print_algo_a
 		if (print_algo_add_info != NULL) print_algo_add_info(1);
 		printf("    3 - Statistical (Hard mode)\n");
 		if (print_algo_add_info != NULL) print_algo_add_info(2);
-		printf("    w - Statistical (Hard mode) (Larger vocabulary)\n");
+		printf("    4 - Statistical (Hard mode) (Larger vocabulary)\n");
 		if (print_algo_add_info != NULL) print_algo_add_info(3);
-		printf("    e - Statistical (No hard mode)\n");
+		printf("    w - Statistical (No hard mode)\n");
 		if (print_algo_add_info != NULL) print_algo_add_info(4);
-		printf("    s - Statistical (No hard mode) (Resilient)\n");
+		printf("    e - Statistical (No hard mode) (Resilient)\n");
 		if (print_algo_add_info != NULL) print_algo_add_info(5);
-		printf("    d - Random guess\n");
+		printf("    r - Statistical (No hard mode) (Slightly Optimised) [WIP]\n");
 		if (print_algo_add_info != NULL) print_algo_add_info(6);
-		printf("    z - Random guess (Larger vocabulary)\n");
+		printf("    a - Statistical (No hard mode) (Resilient) (Slightly Optimised) [WIP]\n");
 		if (print_algo_add_info != NULL) print_algo_add_info(7);
+		printf("    s - Matt Dodge Hybrid (No hard mode) [WIP]\n");
+		if (print_algo_add_info != NULL) print_algo_add_info(8);
+		printf("    d - Matt Dodge Hybrid (No hard mode) (Resilient) [WIP]\n");
+		if (print_algo_add_info != NULL) print_algo_add_info(9);
+		printf("    f - Random guess\n");
+		if (print_algo_add_info != NULL) print_algo_add_info(10);
+		printf("    z - Random guess (Larger vocabulary)\n");
+		if (print_algo_add_info != NULL) print_algo_add_info(11);
 		printf("    q - Exit\n\n");
 		printf(" >> ");
 		free(input);
@@ -112,28 +196,48 @@ int select_algo_page(void (*print_title_stuff)(), int* algo, void (*print_algo_a
 			free(input);
 			return 0;
 		}
-		if (strcmp(input, "w") == 0) {
+		if (strcmp(input, "4") == 0) {
 			*algo = 3;
 			free(input);
 			return 0;
 		}
-		if (strcmp(input, "e") == 0) {
+		if (strcmp(input, "w") == 0) {
 			*algo = 4;
 			free(input);
 			return 0;
 		}
-		if (strcmp(input, "s") == 0) {
+		if (strcmp(input, "e") == 0) {
 			*algo = 5;
 			free(input);
 			return 0;
 		}
-		if (strcmp(input, "d") == 0) {
+		if (strcmp(input, "r") == 0) {
 			*algo = 6;
 			free(input);
 			return 0;
 		}
-		if (strcmp(input, "z") == 0) {
+		if (strcmp(input, "a") == 0) {
 			*algo = 7;
+			free(input);
+			return 0;
+		}
+		if (strcmp(input, "s") == 0) {
+			*algo = 8;
+			free(input);
+			return 0;
+		}
+		if (strcmp(input, "d") == 0) {
+			*algo = 9;
+			free(input);
+			return 0;
+		}
+		if (strcmp(input, "f") == 0) {
+			*algo = 10;
+			free(input);
+			return 0;
+		}
+		if (strcmp(input, "z") == 0) {
+			*algo = 11;
 			free(input);
 			return 0;
 		}
--- src/picking_algorithm/algorithms.h
+++ src/picking_algorithm/algorithms.h
@@ -1,6 +1,34 @@
 #ifndef ALGORITHMS_H_INCLUDED
 #define ALGORITHMS_H_INCLUDED
 
+#include <stdio.h>
+
+#include "../wordle_solver/solver.h"
+
+typedef struct wordle_game_solver solver;
+
+typedef struct algo_registry algorithm;
+
+struct algo_registry {
+	int id;
+	char* name;
+//	char* tag_desc; // Small desc
+	size_t min_word_lists;
+	char* (*suggest_word) (gbucket* guess_board, wlist** word_lists, size_t nword_lists);
+	void (*init) (solver* slvr, algorithm* algo);
+	void (*cleanup) (solver* slvr, algorithm* algo); // Note that every wlist in the solver will be automatically freed after this.
+	void (*guess_made) (solver* slvr, algorithm* algo); // Note that word lists with standard filter enabled will be filtered before this.
+};
+
+typedef struct algo_category_registry {
+	int id;
+	char* name;
+	char* info;
+	algorithm** registered_algorithms;
+	size_t registered_algo_count;
+	size_t registered_algo_alloc;
+} alcat;
+
 int select_algo_page(void (*print_title_stuff)(), int* algo, void (*print_algo_add_info)(int algo));
 
 #endif // ALGORITHMS_H_INCLUDED

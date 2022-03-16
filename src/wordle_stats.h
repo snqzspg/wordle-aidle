1fa834debb8e25786f3821d78a8725b1c906cbd8
Snqzs' PG <43104884+snqzspg@users.noreply.github.com>
2022-03-16 19:11:17 +0800

    Fixed extern syntax

--- src/wordle_stats.h
+++ src/wordle_stats.h
@@ -4,9 +4,9 @@
 #include <stdio.h>
 #include <time.h>
 
-const size_t wordle_word_length;
-const size_t wordle_max_guesses;
+extern const size_t wordle_word_length;
+extern const size_t wordle_max_guesses;
 
-const time_t wordle_word_0;
+extern const time_t wordle_word_0;
 
 #endif // WORDLE_STATS_H_INCLUDED

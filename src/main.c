a97b387f9f7febcd54481c8fd69e1094ad92e8d0
Snqzs' PG <43104884+snqzspg@users.noreply.github.com>
2022-03-16 19:10:33 +0800

    Prep for restructuring

--- src/main.c
+++ src/main.c
@@ -6,23 +6,27 @@
 
 #include "about.h"
 #include "settings/settings.h"
-#include "terminal_helper/ccolours.h"
+#include "terminal_helper/cons_graphics.h"
 #include "word_list/hardcoded_dictionary.h"
+
+//#include "picking_algorithm/algorithms.h"
 
 char colour_blind_mode = 1;
 
 const char* name = "Wordle Aidle";
 const char* innername = "waidle";
-const char* version = "0.3.1.1 Build 202203152244";
+const char* version = "0.3.2 Build 202203161903";
 
-int main() {
+int main(void) {
 	pgcg_init_console_graphics();
 	if (clear_warning_thread()) {
 		return 0;
 	}
 	srand(time(NULL));
 	init_dict();
+//	register_algorithms();
 	homepage_thread();
+//	alcats_clear();
 	cleanup_dict();
 	return 0;
 }

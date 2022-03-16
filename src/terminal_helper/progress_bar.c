1020e50f9a3fafdd08872066d5bd2db70fedb364
Snqzs' PG <43104884+snqzspg@users.noreply.github.com>
2022-03-16 19:25:14 +0800

    Update progress_bar.c

--- src/terminal_helper/progress_bar.c
+++ src/terminal_helper/progress_bar.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "ccolours.h"
+#include "cons_graphics.h"
 
 void print_progress_bar_u(unsigned int progress, unsigned int total, size_t length) {
 	size_t col_length = (size_t) progress * length / (size_t) total;

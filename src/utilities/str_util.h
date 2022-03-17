7ea244e2e8420da7fe518daae27e7a581905c105
Snqzs' PG <43104884+snqzspg@users.noreply.github.com>
2022-03-17 23:36:16 +0800

    Update str_util.h

--- src/utilities/str_util.h
+++ src/utilities/str_util.h
@@ -2,6 +2,7 @@
 #define STR_UTIL_H_INCLUDED
 
 char str_islen(const char *s, size_t len);
+void lowercase(char* s);
 void insert_int_bef_ext(char* fname, const int n, const size_t buflen);
 
 #endif // STR_UTIL_H_INCLUDED

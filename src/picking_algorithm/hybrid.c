8082450c3af2b6a98df9c274b18b73e304add903
Snqzs' PG <43104884+snqzspg@users.noreply.github.com>
2022-05-24 13:27:47 +0800

    Update hybrid.c

--- src/picking_algorithm/hybrid.c
+++ src/picking_algorithm/hybrid.c
@@ -25,12 +25,12 @@ void matt_dodge_hard_larger_init(solver* slvr, algorithm* algo) {
 }
 
 //char* guess_by_information_freq_hybrid(wlist* main_list, gbucket* g, wlist* alt_list) {
-char* guess_by_information_freq_hybrid(gbucket* guess_board, wlist** word_lists, size_t nword_lists) {
+char* guess_by_information_freq_hybrid(gbucket* guess_board, wlist** word_lists, size_t nword_lists, char show_word_list_to_user) {
 	if (word_lists[0] -> length == 0) {
 		return NULL;
 	}
 	if (word_lists[0] -> length < 50) {
-		return guess_by_information_entropy(guess_board, word_lists, nword_lists);
+		return guess_by_information_entropy(guess_board, word_lists, nword_lists, show_word_list_to_user);
 	}
-	return guess_by_freq_cols(guess_board, word_lists, nword_lists);
+	return guess_by_freq_cols(guess_board, word_lists, nword_lists, show_word_list_to_user);
 }

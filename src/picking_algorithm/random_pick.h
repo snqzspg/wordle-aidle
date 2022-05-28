1e2583cd9a60c87d66ec75b69691c3d09095de22
Snqzs' PG <43104884+snqzspg@users.noreply.github.com>
2022-05-28 13:46:05 +0800

    Update random_pick.h

--- src/picking_algorithm/random_pick.h
+++ src/picking_algorithm/random_pick.h
@@ -5,12 +5,13 @@
 #include "../wordle/word_list.h"
 
 void random_pick_init(solver* slvr, algorithm* algo);
+void random_pick_ignore_clues_init(solver* slvr, algorithm* algo);
 void random_pick_larger_init(solver* slvr, algorithm* algo);
+void random_pick_ignore_clues_larger_init(solver* slvr, algorithm* algo);
 
 /**
  * Self explanatory
  */
-//char* guess_randomly(wlist* word_list, gbucket* g, wlist* alt_list);
 char* guess_randomly(gbucket* guess_board, wlist** word_lists, size_t nword_lists, char show_word_list_to_user);
 
 #endif // RANDOM_PICK_H_INCLUDED

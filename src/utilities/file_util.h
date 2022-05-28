c351bdbec7787e4c7c89ae6e0db0c2b2f4bf6e30
Snqzs' PG <43104884+snqzspg@users.noreply.github.com>
2022-05-28 13:40:15 +0800

    Update file_util.h

--- src/utilities/file_util.h
+++ src/utilities/file_util.h
@@ -1,9 +1,14 @@
 #ifndef FILE_UTIL_H_INCLUDED
 #define FILE_UTIL_H_INCLUDED
 
+
 /**
  * Delete using free
  */
 char* create_log_filename(const char* prefix);
+
+char get_file_seperator();
+char* find_file_sep(const char* path);
+char* get_path_leaf(const char* path);
 
 #endif // FILE_UTIL_H_INCLUDED

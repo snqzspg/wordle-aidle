8adeb7fd3ae2b2aa89fc80ce53b5b80cc3fad3ee
Snqzs' PG <43104884+snqzspg@users.noreply.github.com>
2022-05-28 13:47:33 +0800

    Update homepage.h

--- src/pages/homepage.h
+++ src/pages/homepage.h
@@ -2,7 +2,7 @@
 #define HOMEPAGE_H_INCLUDED
 
 void print_title_banner();
-char clear_warning_thread();
+char clear_warning_thread(const char* arg0);
 int homepage_thread();
 
 #endif // HOMEPAGE_H_INCLUDED

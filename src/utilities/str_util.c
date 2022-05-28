db56e299d76ff7b5dda7456363e55f4642b8292d
Snqzs' PG <43104884+snqzspg@users.noreply.github.com>
2022-05-28 13:39:00 +0800

    Update str_util.c

--- src/utilities/str_util.c
+++ src/utilities/str_util.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "hash_util.h"
 #include "int_util.h"
 #include "str_util.h"
 
@@ -37,31 +38,6 @@ void uppercase(char* s) {
 	}
 }
 
-//const char querty_key_seq[36] = "123weasdzxc456rtyfghvbn789uiojklm0p";
-//
-///**
-// * Note that first option starts from 0
-// * Returns the length of the corresponding option key string of i
-// */
-//size_t get_option_key_len(size_t i) {
-//	return i / 35 + 1;
-//}
-//
-///**
-// * Note that first option starts from 0
-// * Copies the corresponding option key of i into opt
-// */
-//void cpy_option_key(size_t i, char* optbuf) {
-//	while (i >= 35) {
-//		*optbuf = querty_key_seq[i % 35];
-//		optbuf++;
-//		i /= 35;
-//	}
-//	*optbuf = querty_key_seq[i % 35];
-//	optbuf++;
-//	*optbuf = '\0';
-//}
-
 void insert_int_bef_ext(char* fname, const int n, const size_t buflen) {
 	const size_t nlen = num_char_int(n);
 	const size_t slen = strlen(fname);
@@ -77,48 +53,51 @@ void insert_int_bef_ext(char* fname, const int n, const size_t buflen) {
 }
 
 long hash_str(const char* s) {
-	register size_t len;
-	register long x;
-
-	len = strlen(s);
-	x = *s << 7;
-	while (--len >= 10) {
-		x = (1000003 * x) ^ (*s + 1);
-	}
-	x ^= strlen(s);
-	if (x == -1) {
-		x = -2;
-	}
-	return x;
+	return hash_bytes(s, strlen(s) * sizeof(char));
+//	register size_t len;
+//	register long x;
+//
+//	len = strlen(s);
+//	x = *s << 7;
+//	while (--len >= 10) {
+//		x = (1000003 * x) ^ (*s + 1);
+//	}
+//	x ^= strlen(s);
+//	if (x == -1) {
+//		x = -2;
+//	}
+//	return x;
 }
 
 /**
  * http://www.cse.yorku.ca/~oz/hash.html
  */
 long djb2_hash_str(const char* s) {
-	if (*s == '\0') {
-		return 0;
-	}
-	long hash = 5381;
-	int c = *s;
-	while (c) {
-		hash = ((hash << 5) + hash) + c;
-		c = *s;
-		s++;
-	}
-	return hash;
+	return djb2_hash_bytes(s, strlen(s) * sizeof(char));
+//	if (*s == '\0') {
+//		return 0;
+//	}
+//	long hash = 5381;
+//	int c = *s;
+//	while (c) {
+//		hash = ((hash << 5) + hash) + c;
+//		c = *s;
+//		s++;
+//	}
+//	return hash;
 }
 
 /**
  * http://www.cse.yorku.ca/~oz/hash.html
  */
 long sdbm_hash_str(const char* s) {
-	long hash = 0;
-	int c = *s;
-	while (c) {
-		hash = c + (hash << 6) + (hash << 16) - hash;
-		c = *s;
-		s++;
-	}
-	return hash;
+	return sdbm_hash_bytes(s, strlen(s) * sizeof(char));
+//	long hash = 0;
+//	int c = *s;
+//	while (c) {
+//		hash = c + (hash << 6) + (hash << 16) - hash;
+//		c = *s;
+//		s++;
+//	}
+//	return hash;
 }
